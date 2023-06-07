2fbabe1fc07fb5f0a7dd91f98e7f4fc458a12c0c
gabielsouzaR <souza.ramos@aluno.ifsp.edu.br>
2023-06-07 20:09:23 -0300

    atualização 07/06/23

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ const message = document.querySelector(".message");
 buttonYes.addEventListener("click", function () {
   message.innerHTML = "TE AMO❤️";
 });
+
+button.addEventListener("click", function () {
+  const containerWidth = document.querySelector(".container").offsetWidth;
+  const containerHeight = document.querySelector(".container").offsetHeight;
+  const buttonWidth = button.offsetWidth;
+  const buttonHeight = button.offsetHeight;
+
+  const randomX = Math.random() * (containerWidth - buttonWidth);
+  const randomY = Math.random() * (containerHeight - buttonHeight);
+
+  button.style.top = randomY + "px";
+  button.style.left = randomX + "px";
+});
